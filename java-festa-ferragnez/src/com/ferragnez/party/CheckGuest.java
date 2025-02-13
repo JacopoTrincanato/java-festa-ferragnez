d098fa42f2b2283b11cd1fa4090072dd0445aca9
JacopoTrincanato <trincanatoj@gmail.com>
2025-02-13 18:32:32 +0100

    cycle into the array and find if the name is on the list

--- java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
+++ java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
@@ -13,7 +13,29 @@ public class CheckGuest {
         // creo una variabile per contenere lo scanner
         Scanner scanner = new Scanner(System.in);
 
+        // chiedo all'utente come si chiama
+        System.out.println("Buonasera, come ti chiami?");
+        String nomeInvitato = scanner.nextLine();
+
+        boolean invitatoPresente = false;
         // ciclo nell'array per cercare il nome nella lista
+        for (int i = 0; i < invitati.length; i++) {
+
+            // se il nome è presente nella lista, allora lascio entrare l'invitato
+            if (invitati[i].equals(nomeInvitato)) {
+                invitatoPresente = true;
+            }
+
+            if (invitatoPresente) {
+                System.out.println("Puoi entrare alla festa");
+            } else {
+                System.out.println("non puoi entrare alla festa. Non sei sulla lista degli invitati");
+            }
+
+        }
+
+        // chiudo lo scanner
+        scanner.close();
 
     }
 }
