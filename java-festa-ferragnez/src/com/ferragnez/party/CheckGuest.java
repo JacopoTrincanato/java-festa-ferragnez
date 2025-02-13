d1454dcbb3d55d0c95f421f5f581d8cdd432e90d
JacopoTrincanato <trincanatoj@gmail.com>
2025-02-13 17:51:29 +0100

    create a variable for the scanner and a variable for guests

--- java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
+++ java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,19 @@
 package com.ferragnez.party;
 
+//importo lo scanner
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
+
+        // creo l'array con il nome degli invitati
+        String[] invitati = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+
+        // creo una variabile per contenere lo scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // ciclo nell'array per cercare il nome nella lista
 
     }
 }
